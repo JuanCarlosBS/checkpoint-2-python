fefb35e0193d77053057ca372f29c03a743a840e
Juan Carlos <juancarlosbs>
2020-09-22 19:48:49 -0300

    feat: add create store function and logout

--- main.py
+++ main.py
@@ -10,16 +10,15 @@ menu = 0
 while menu != 7:
     print('\nMenu')
     menu = int(input(
-        "\t1. Cadastrar Loja\n\t2. Listar Produtos por Loja\n\t3. Cadastrar Lojas\n\t4. Sair"))
-
+        "\t1. Cadastrar Loja\n\t2. Listar Produtos por Loja\n\t3. Cadastrar Lojas\n\t4. Sair\n"))
+        
     if menu == 1:
-
+        store.create()
     elif menu == 2:
-
+        print("opção 2")
     elif menu == 3:     
-
+        print("opção 3")
     elif menu == 4:
         authentication.logout()
-        
     else:
         print("\033[31mOPÇÃO INVALIDA!\033[m")
--- store.py
+++ store.py
@@ -1,1 +1,32 @@
 import settings
+
+
+def create() :
+    cont = 0
+    while cont < 6:
+        storeIsValid = False
+
+        while storeIsValid != True:
+            id = int(input('ESCREVA O CÓDIGO DA LOJA: '))
+            found_item = list(filter(lambda _product: _product['id'] == id, settings.store))
+            if len(found_item) > 0:
+                print('CÓDIGO JÁ EXISTENTE.')
+            elif id == 0:
+                print('CÓDIGO DEVE SER DIFERENTE DE 0')
+            else:
+                storeIsValid = True
+
+        name = input('ESCREVA O NOME DA LOJA: ')
+
+        store = {
+            'id' : id,
+            'name' : name
+        }
+
+        settings.store.append(store)
+
+        print('PRODUTO CADASTRADO COM SUCESSO.')
+
+        cont += 1
+
+    return settings.store
