7cc3ee55be1f639825116e614a9d48b27b6e7cde
juancarlosbs <juanrov_@hotmail.com>
2020-10-20 21:39:44 -0300

    refact: ready code to production

--- settings.py
+++ settings.py
@@ -8,52 +8,6 @@ def init() :
 
 
     menu = 0 
-    store = [
-        {
-            'id' : 123,
-            'name' : 'Casas Bahia'
-        },
-        {
-            'id' : 456,
-            'name' : 'Marisa'
-        },
-        {
-            'id' : 789,
-            'name' : 'Marabras'
-        },
-        {
-            'id' : 98,
-            'name' : 'AliExpress'
-        },
-        {
-            'id' : 765,
-            'name' : 'Amazon'
-        },
-        {
-            'id' : 432,
-            'name' : 'Ponto Frio'
-        }
-    ]
-    storage = [
-        {
-            'id' : 1001,
-            'price': [10,15,20,25,30,35],
-        },
-        {
-            'id' : 1002,
-            'price': [3,25,23,32,15,150],
-        },
-        {
-            'id' : 1003,
-            'price': [140,180,234,150,230,116],
-        },
-        {
-            'id' : 1004,
-            'price': [32,43,22,76,45,34],
-        },
-        {
-            'id' : 1005,
-            'price': [19,34,87,76,34,54],
-        }
-    ]
+    store = []
+    storage = []
     possibleCreateStore = True
